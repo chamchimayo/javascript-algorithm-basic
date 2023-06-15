fcd768073084ed5a62e8c848d9acd6ef72ec9306
chamchimayo <chamchi.maayo@gmail.com>
2023-06-15 21:21:39 +0900

    2023-06-15 프로그래머스 12925 문자열을 정수로 바꾸기

---
+++ programmers/prg12925_문자열을정수로바꾸기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    var answer = Number(s);
+    return answer;
+}
+
+// 다른 사람의 풀이
+// function strToInt(str) {
+//     return str / 1
+// }
+
+// function strToInt(str) {
+//     return +str;
+// }
